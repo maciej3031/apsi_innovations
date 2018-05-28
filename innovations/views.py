58f30dab5ac946b040c19cb220e738484219e792
delfinagolaszewska <delfinagolaszewska@gmail.com>
2018-05-28 21:16:32 +0200

    Merge branch 'tsk_43_edit_innovation' of /home/delfina/PycharmProjects/APSI/apsi_innovations with conflicts.

--- innovations/views.py
+++ innovations/views.py
@@ -1,20 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db import transaction
-from django.http import Http404
-
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.utils.timezone import now
-from django.views.generic import CreateView, UpdateView
+from django.template import loader
+from django.views.generic import CreateView
 
-from innovations.forms import InnovationAddForm, GradeForm, ReportViolationForm
+from innovations.forms import GradeForm, ReportViolationForm, InnovationAddForm, AppraiseForm
 from innovations.models import Innovation, Keyword, InnovationUrl, InnovationAttachment, Grade, ViolationReport
 from signup.groups import administrators, committee_members, in_groups, students, in_group, employees
 from socials.models import Comment, SocialPost
-
-from django.http import HttpResponse
-from django.template import loader
 
 
 class InnovationAddView(SuccessMessageMixin, CreateView):
@@ -38,27 +35,17 @@ class InnovationAddView(SuccessMessageMixin, CreateView):
         return super().form_valid(form)
 
 
-class InnovationUpdateView(SuccessMessageMixin, UpdateView):
-    model = Innovation
-    template_name = "add_innovation.html"
+class InnovationAppraiseView(SuccessMessageMixin, CreateView):
+    template_name = 'innovations/appraise.html'
+    form_class = AppraiseForm
     success_url = '/'
-    success_message = "%(subject)s was successfully updated"
-    fields = ['subject', 'description', 'assumptions', 'benefits', 'costs']
-
-    def dispatch(self, request, *args, **kwargs):
-        innovation = get_object_or_404(Innovation, id=kwargs['pk'])
-        user_is_not_owner = innovation.issuer != request.user
-        replenishment_not_needed = innovation.status != Innovation.Status.IN_REPLENISHMENT
-        if user_is_not_owner or replenishment_not_needed:
-            return render(request, "permission_denied.html")
-        else:
-            return super(InnovationUpdateView, self).dispatch(request, *args, **kwargs)
+    success_message = "You appraised successfully. Thank you!"
 
     @transaction.atomic
     def form_valid(self, form):
-        form.instance.status = Innovation.Status.PENDING
-        form.instance.save()
-        return super(InnovationUpdateView, self).form_valid(form)
+        form.instance.issuer = self.request.user
+        form.instance.innovation = Innovation.objects.get(id=self.kwargs['id'])
+        return super().form_valid(form)
 
 
 @method_decorator(login_required, name='dispatch')
@@ -108,12 +95,38 @@ def innovations(request):
     return render(request, "innovations/innovations_list.html", {"innovations": innovations})
 
 
+def calculate_innovation_grade(innovation):
+    aggregate_grade = 0
+    grades = Grade.objects.get(innovation_id=innovation.id)
+    if not grades:
+        return None
+    number_of_votes = len(grades)
+    for grade in grades:
+        user_weight = get_user_grade_weight(grade.user_id, innovation)
+        aggregate_grade += calculate_single_grade(user_weight, grade.value)
+    return aggregate_grade/number_of_votes
+
+
+def calculate_single_grade(user_weight, user_grade):
+    return float(user_weight * user_grade)
+
+
+def get_user_grade_weight(user, innovation):
+    if user.group_id == 1:
+        return innovation.student_grade_weight
+    elif user.group_id == 3:
+        return innovation.employee_grade_weight
+    else:
+        raise ValueError('Grade given by user without privileges')
+
+
 @login_required
 def single(request, id):
     innovation = get_object_or_404(Innovation, id=id)
     if is_forbidden(innovation.status, request.user):
         raise Http404("Page not found!")
-    return render(request, "innovations/innovations_list.html", {"innovations": [innovation]})
+    grade = calculate_innovation_grade(innovation)
+    return render(request, "innovations/innovations_list.html", {"innovations": [innovation], "grade": [grade]})
 
 
 @login_required
@@ -167,6 +180,31 @@ def finish_violation_report(request):
     return redirect("reported_violations")
 
 
+@login_required
+def set_status_substantiation(request, id, status_substantiation):
+    if not has_confidential_access(request.user):
+        raise render(request, "permission_denied.html")
+    Innovation.objects.filter(id=id).update(status_substantiation=status_substantiation)
+    return redirect("single", id=id)
+
+
+def appraise(request, id):
+    innovation = get_object_or_404(Innovation, id=id)
+    if has_appraise_access(request.user, innovation):
+        if request.method == 'GET':
+            form = AppraiseForm()
+            return render(request, "innovations/appraise.html", {"form": form})
+        if request.method == "POST":
+            form = AppraiseForm(data=request.POST)
+            if form.is_valid():
+                Innovation.objects.filter(id=id)\
+                    .update(status_substantiation=form.cleaned_data.get('status_substantiation'),
+                            status=form.cleaned_data.get('status'))
+            return redirect("single", id=id)
+    else:
+        return render(request, "permission_denied.html")
+
+
 def is_forbidden(status, user):
     return is_confidential(status) and not has_confidential_access(user)
 
@@ -203,49 +241,58 @@ def all_innovation_statuses():
         for status in dir(Innovation.Status)
         if isinstance(status, str) and not status.startswith("__")
     ]
-	
-def innovation_list(request):
-	innovations = Innovation.objects.filter(status = 'voting').order_by('timestamp')
-	suspended = Innovation.objects.filter(status = 'suspended').order_by('timestamp')
-	pending = Innovation.objects.filter(status = 'pending').order_by('timestamp')
-	blocked = Innovation.objects.filter(status = 'blocked').order_by('timestamp')
-	details = Innovation.objects.filter(status = 'in_replenishment').order_by('timestamp')
-	template = loader.get_template('innovation_list.html')
-	context = {
-		'innovations': innovations,
-		'suspended': suspended,
-		}
-	return HttpResponse(template.render(context, request))
-	
-def rejected_list(request):
-	rejected = Innovation.objects.filter(status='rejected').order_by('timestamp')
-	approved = Innovation.objects.filter(status = 'accepted').order_by('timestamp')
-	template = loader.get_template('closed_list.html')
-	context = {
-		'rejected': rejected,
-		'approved': approved,
-		}
-	return HttpResponse(template.render(context, request))
-	
-def admin_list(request):
-	pending = Innovation.objects.filter(status = 'pending').order_by('timestamp')
-	blocked = Innovation.objects.filter(status = 'blocked').order_by('timestamp')
-	details = Innovation.objects.filter(status = 'IN_REPLENISHMENT').order_by('timestamp')
-	template = loader.get_template('admin_list.html')
-	context = {
-		'blocked': blocked,
-		'pending': pending,
-		'details': details,
-		}
-	return HttpResponse(template.render(context, request))
-	
-def detail(request, idea_id):
-	template = loader.get_template('innovation_detail.html')
-	idea = Innovation.objects.filter(id=idea_id)
-	comments = Grade.objects.filter(innovation_id = idea_id)
 
-	context = {
-		'idea': idea,
-		'comments': comments,
-		}
-	return HttpResponse(template.render(context, request))
+
+def innovation_list(request):
+    innovations = Innovation.objects.filter(status='voting').order_by('timestamp')
+    suspended = Innovation.objects.filter(status='suspended').order_by('timestamp')
+    pending = Innovation.objects.filter(status='pending').order_by('timestamp')
+    blocked = Innovation.objects.filter(status='blocked').order_by('timestamp')
+    details = Innovation.objects.filter(status='in_replenishment').order_by('timestamp')
+    template = loader.get_template('innovation_list.html')
+    context = {
+        'innovations': innovations,
+        'suspended': suspended,
+        }
+    return HttpResponse(template.render(context, request))
+
+
+def rejected_list(request):
+    rejected = Innovation.objects.filter(status='rejected').order_by('timestamp')
+    approved = Innovation.objects.filter(status='accepted').order_by('timestamp')
+    template = loader.get_template('closed_list.html')
+    context = {
+        'rejected': rejected,
+        'approved': approved,
+        }
+    return HttpResponse(template.render(context, request))
+
+
+def admin_list(request):
+    pending = Innovation.objects.filter(status='pending').order_by('timestamp')
+    blocked = Innovation.objects.filter(status='blocked').order_by('timestamp')
+    details = Innovation.objects.filter(status='IN_REPLENISHMENT').order_by('timestamp')
+    template = loader.get_template('admin_list.html')
+    context = {
+        'blocked': blocked,
+        'pending': pending,
+        'details': details,
+        }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, idea_id):
+    template = loader.get_template('innovation_detail.html')
+    idea = Innovation.objects.filter(id=idea_id)
+    comments = Grade.objects.filter(innovation_id=idea_id)
+    context = {
+        'idea': idea,
+        'comments': comments,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def has_appraise_access(user, innovation):
+    has_appraise_status = innovation.status in [Innovation.Status.VOTING]
+    has_appraise_privileges = (in_groups(user, [committee_members]) and innovation.employee_grade_weight)
+    return has_appraise_status and has_appraise_privileges
